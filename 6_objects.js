0dded0eea6bf3837c540434c1ab3de5d0b25f657
SpawnMetal <spawnmetal666@gmail.com>
2022-07-27 11:17:49 +0700

    Updated 2022.07.27

--- 6_objects.js
+++ 6_objects.js
@@ -1,23 +1,21 @@
 const cityField = 'city'
 const job = 'Frontend'
 
-const person =
-{
-    age: 26,
-    name: 'Irina',
-    job,
-    [cityField]: 'Limassol', //Будет динамический ключ city
-    [cityField + Date.now()]: 'Limassol', //Будет динамический ключ city...
-    'country-live': 'Cyprus',
-    print: () => 'Person', //Не работает с контекстом this
-    // toString: function()
-    toString() //То же самое, только работает с контекстом this в отличае от стрелочной
-    {
-        return Object.keys(this)
-        .filter(key => key !== 'toString')
-        .map(key => this[key])
-        .join(' ')
-    }
+const person = {
+  age: 26,
+  name: 'Irina',
+  job,
+  [cityField]: 'Limassol', //Будет динамический ключ city
+  [cityField + Date.now()]: 'Limassol', //Будет динамический ключ city...
+  'country-live': 'Cyprus',
+  print: () => 'Person', //Не работает с контекстом this
+  // toString: function()
+  toString() { //То же самое, только работает с контекстом this в отличае от стрелочной
+    return Object.keys(this)
+      .filter(key => key !== 'toString')
+      .map(key => this[key])
+      .join(' ')
+  },
 }
 
 console.log(person.toString())
@@ -37,7 +35,7 @@ console.log(first)
 const obj = Object.assign({}, first, {c: 2, d: 3})
 console.log(obj)
 
-console.log(Object.entries(obj)) //Ассицциативный массив, может например портироваться в php
+console.log(Object.entries(obj)) //Ассоциативный массив, может например портироваться в php
 // (3) [Array(2), Array(2), Array(2)]
 // 0: (2) ["a", 1]
 // 1: (2) ["c", 2]
